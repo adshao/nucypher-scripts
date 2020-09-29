906396400a802336e073a9c10de700756eba6d22
adshao <tjusgj@gmail.com>
2020-09-30 00:36:56 +0800

    Init repo
    Signed-off-by: adshao <tjusgj@gmail.com>

---
+++ init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# CentOS
+
+yum install -y yum-utils
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install -y wget tmux iptables git python38 python3-devel
+yum install -y docker-ce docker-ce-cli containerd.io iptables
+iptables -t nat -N DOCKER
+
+systemctl start docker
+systemctl enable docker
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py
+pip install docker-compose
+
+mkdir -p /root/geth/data /root/clef/data /root/nucypher/data
+
+# alias geth='docker-compose run --rm geth /usr/local/bin/geth'
+# alias clef='docker-compose run --rm clef'
+# alias nucypher='docker-compose run --rm nucypher nucypher'
+
+# clef init
+# clef
+# docker-compose up -d geth
+
